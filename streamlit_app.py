0069956183e33d95367d1281c08046357c861db3
porkupine100 <porkupine100@sbcglobal.net>
2022-12-09 11:00:53 -0800

    theme update

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,5 +76,6 @@ response = AgGrid(
     enable_enterprise_modules=True,
     update_mode=GridUpdateMode.MODEL_CHANGED,
     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-    fit_columns_on_grid_load=False,
+    fit_columns_on_grid_load=True,
+    theme = 'dark'
 )

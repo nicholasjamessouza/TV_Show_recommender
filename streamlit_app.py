60a3af7bb31764b04b3d093738402a23d510d2a6
porkupine100 <porkupine100@sbcglobal.net>
2022-12-09 12:38:20 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ df['themes']=df['themes'].apply(lambda x: x.replace('[','').replace(']','').repl
 gb = GridOptionsBuilder.from_dataframe(df[['grid_title','popularity','score','genres','themes']].reset_index(drop=True))
 gb.configure_column("grid_title",cellRenderer=JsCode('''function(params) {return params.value}'''))
 # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
+gb.configure_default_column(enablePivot=False, enableValue=False, enableRowGroup=False)
 gb.configure_selection(selection_mode="multiple", use_checkbox=False)
 gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
 gridOptions = gb.build()

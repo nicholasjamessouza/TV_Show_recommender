664fbf15ce1ac2956d1b7015f4c23bbd60121b5f
porkupine100 <porkupine100@sbcglobal.net>
2022-12-09 11:48:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,10 +60,10 @@ from st_aggrid import GridUpdateMode, DataReturnMode, JsCode
 
 
 gb = GridOptionsBuilder.from_dataframe(df[['grid_title','popularity','score','genres','themes']].reset_index(drop=True))
-gb.configure_column("grid_title",
-                            headerName="Link",
-                            cellRenderer=JsCode('''function(params) {return params.value}'''),
-                            width=300)
+cell_renderer =  JsCode("""
+function(params) {return `${params.value}`}
+""")
+gb.configure_column("grid_title",cellRenderer=cell_renderer)
 # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
 gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
 gb.configure_selection(selection_mode="multiple", use_checkbox=True)
